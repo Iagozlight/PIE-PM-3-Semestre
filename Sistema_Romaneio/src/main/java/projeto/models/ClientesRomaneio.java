b5d0644c09f1131562e24376816f089a5d12a859
Thales <thalesantos12@gmail.com>
2026-04-04 23:58:15 -0300

    Escopo da parte Romaneios criada, ainda falta adaptar funções

--- Sistema_Romaneio/src/main/java/projeto/models/ClientesRomaneio.java
+++ Sistema_Romaneio/src/main/java/projeto/models/ClientesRomaneio.java
@@ -1,10 +1,11 @@
-package projeto;
+package projeto.models;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity(name = "clientes_romaneio")
 public class ClientesRomaneio {
 
     @Id
@@ -18,13 +19,92 @@ public class ClientesRomaneio {
     private String rua;
 
     @Column(name = "numero")
-    private int numero;
+    private String numero;
 
     @Column(name = "bairro")
     private String bairro;
 
     @Column(name = "cep")
-    private int cep;
+    private String cep;
 
+    @OneToMany(mappedBy = "clientes", cascade = CascadeType.ALL)
+    private List<Pedidos> pedidos = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "romaneios_id", nullable = false)
+    private Romaneios romaneio;
+
+    public ClientesRomaneio() {}
+
+    public ClientesRomaneio(Long id, String nome_cliente, String rua, String numero,
+                            String bairro, String cep)
+    {
+        this.id = id;
+        this.nome_cliente = nome_cliente;
+        this.rua = rua;
+        this.numero = numero;
+        this.bairro = bairro;
+        this.cep = cep;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome_cliente() {
+        return nome_cliente;
+    }
+
+    public void setNome_cliente(String nome_cliente) {
+        this.nome_cliente = nome_cliente;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public List<Pedidos> getPedidos() { return pedidos; }
+    public void setPedidos(List<Pedidos> pedidos) { this.pedidos = pedidos; }
+
+    @Override
+    public String toString() {
+        return "ClientesRomaneio{id=" + id + ", nome='" + nome_cliente + "', cep='" + cep + "', rua='" + rua
+                + "', numero=" + numero + "', bairro='" + bairro + "', pedidos='" + pedidos + "}";
+    }
+
+    public Romaneios getRomaneio() { return romaneio; }
+    public void setRomaneio(Romaneios romaneio) { this.romaneio = romaneio; }
 
 }
--- Sistema_Romaneio/src/main/java/projeto/models/Romaneios.java
+++ Sistema_Romaneio/src/main/java/projeto/models/Romaneios.java
@@ -1,6 +1,10 @@
-package projeto;
+package projeto.models;
 
 import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity(name = "romaneios")
 public class Romaneios {
@@ -9,8 +13,41 @@ public class Romaneios {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "data")
+    private LocalDate data;
 
+    @OneToMany(mappedBy = "romaneio", cascade = CascadeType.ALL)
+    private List<ClientesRomaneio> clientes = new ArrayList<>();
 
+    public Romaneios() {}
 
+    public Romaneios(Long id, LocalDate data) {
+        this.id = id;
+        this.data = data;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public List<ClientesRomaneio> getClientes() { return clientes; }
+    public void setClientes(List<ClientesRomaneio> clientes) { this.clientes = clientes; }
+
+    @Override
+    public String toString() {
+        return "Romaneio{id=" + id + ", data=" + data + ", clientes=" + clientes + "}";
+    }
 
 }
