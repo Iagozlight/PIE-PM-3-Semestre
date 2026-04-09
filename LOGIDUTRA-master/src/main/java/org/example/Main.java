0c2f695844fbc64a46f3306fdf56464d04149428
Mateussantos1207 <mateussntos99@gmail.com>
2026-04-09 20:58:28 -0300

    Funçao de novo motorista e o repositorio dos motoristas

--- LOGIDUTRA-master/src/main/java/org/example/Main.java
+++ LOGIDUTRA-master/src/main/java/org/example/Main.java
@@ -1,17 +1,30 @@
 package org.example;
+import org.example.Repository.Motoristasrepository;
+import org.example.config.FlyWayconfig;
+import org.example.models.Motoristas;
 import org.example.models.Usuarios;
 import org.example.repositories.CustomizerFactory;
 import org.example.Repository.UsuarioRepository;
 import jakarta.persistence.EntityManager;
+import org.flywaydb.core.Flyway;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        FlyWayconfig.migrate();
         EntityManager em = CustomizerFactory.getEntityManager();// aqui cria o entitymanager que vai conversar com o banco
         UsuarioRepository usuarioRepository = new UsuarioRepository(em);// o "em" é o EntityManager que será usado pelo repository para manipular o banco de dados
+        Motoristasrepository motoristasrepository = new Motoristasrepository(em);
+
         // chamando os metodos
+
         novoUsuario(usuarioRepository);
+
+        novoMotorista(motoristasrepository,usuarioRepository);
 
         exibirUsuarios(usuarioRepository);
 
@@ -34,7 +47,12 @@ public class Main {
             }
             if (condição.equals("C") || condição.equals("c")) {
                 Usuarios novo = new Usuarios();
+                Motoristas motoristas = new Motoristas();
+
                 System.out.println("\nCADASTRO DE USUARIO");
+
+                System.out.println("Nome: ");
+                motoristas.setNome(sc.nextLine());
 
                 System.out.println("Usuario: ");
                 novo.setUsuario(sc.nextLine());
@@ -50,8 +68,69 @@ public class Main {
         }
     }
 
+    static  void novoMotorista ( Motoristasrepository motoristasrepository, UsuarioRepository usuarioRepository) {
+        Scanner sc = new Scanner(System.in);
+        Motoristas motoristas = new Motoristas();
+
+        Usuarios cadastrado = null;
+
+        System.out.println("Nome do motorista: ");
+        motoristas.setNome(sc.nextLine());
+
+        System.out.println("Data de nascimento: (ex DD/MM/AAAA)");
+        String dataStr = sc.nextLine();
+        LocalDate data = LocalDate.parse(dataStr, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        motoristas.setData_nascimento(data);
+
+        List<Usuarios> lista = usuarioRepository.findAll();
+
+        System.out.println("Nome de Usuario desse motorista: ");
+        String user = sc.nextLine();
+
+        for (Usuarios u : lista) {
+            if (u.getUsuario().equals(user)){
+                cadastrado = u;
+            }
+        }
+        if (cadastrado != null) {
+            if (cadastrado.getUsuario().equals(user)) {
+                System.out.println("Motorista encontrado!!");
+            }
+
+            motoristas.setUsuarios(cadastrado);
+
+            try {
+                motoristasrepository.create(motoristas);
+                System.out.println("Motorista cadastrado com sucesso!");
+            } catch (Exception e) {
+                if (e.getMessage().contains("Motorista deve ter mais de 24 anos")) {
+                    System.out.println("Cadastro negado: pela questão do seguro, o motorista deve ter mais de 24 anos!");
+                }
+            }
+
+        }else {
+            String opcao;
+            do {
+                System.out.println("Motorista nao encontrado, necessita cadastrar como usuario primeiron\nSelecione uma opção:");
+                System.out.println("1- Cadastro de usuarios\n0-Sair");
+                opcao = sc.nextLine();
+                switch (opcao) {
+                    case "1":
+                        novoUsuario(usuarioRepository);
+                        novoMotorista(motoristasrepository, usuarioRepository);
+                        break;
+                    case "0":
+                        return;
+                    default:
+                        System.out.println("Opçao invalida!!");
+                }
+            }
+            while (!opcao.equals("0"));
+        }
+    }
+
     static void exibirUsuarios(UsuarioRepository usuarioRepository) {
-        List<Usuarios> lista = usuarioRepository.findAll();//faz a busca de todos os usuarios de todos os usuarios do banco de dados e retorna uma lista
+        List<Usuarios> lista = usuarioRepository.findAll();
         for (Usuarios u : lista) {
             System.out.println("Usuario: " + u.getUsuario());
         }
