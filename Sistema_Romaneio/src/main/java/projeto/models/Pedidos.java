b73ca3778c24bdf810d71f33a9320ed282152ca3
Thales <thalesantos12@gmail.com>
2026-04-09 23:55:12 -0300

    mudando classe Pedidos adicionando getter e setter para ClientesRomaneios

--- Sistema_Romaneio/src/main/java/projeto/models/Pedidos.java
+++ Sistema_Romaneio/src/main/java/projeto/models/Pedidos.java
@@ -13,9 +13,6 @@ public class Pedidos {
     @Column(name = "nome_produto")
     private String nome_produto;
 
-    @Column(name = "valor")
-    private Double valor;
-
     @Column(name = "quantidade")
     private String quantidade;
 
@@ -25,17 +22,15 @@ public class Pedidos {
 
     public Pedidos() {}
 
-    public Pedidos(Long id, String nome_produto, Double valor, String quantidade) {
+    public Pedidos(Long id, String nome_produto, String quantidade) {
         this.id = id;
         this.nome_produto = nome_produto;
-        this.valor = valor;
         this.quantidade = quantidade;
     }
 
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
@@ -43,31 +38,22 @@ public class Pedidos {
     public String getNome_produto() {
         return nome_produto;
     }
-
     public void setNome_produto(String nome_produto) {
         this.nome_produto = nome_produto;
-    }
-
-    public Double getValor() {
-        return valor;
-    }
-
-    public void setValor(Double valor) {
-        this.valor = valor;
     }
 
     public String getQuantidade() {
         return quantidade;
     }
-
     public void setQuantidade(String quantidade) {
         this.quantidade = quantidade;
     }
 
+    public String getClientes() { return clientes; }
+    public void setClientes(ClientesRomaneio clientes) { this.clientes = clientes; }
+
     @Override
     public String toString() {
-        return "Pedido{id=" + id + ", nome_produto='" + nome_produto +
-                "', valor='" + valor + "', quantidade='" + quantidade + "}";
+        return "Pedido{id=" + id + ", nome_produto='" + nome_produto + "', quantidade='" + quantidade + "}";
     }
-
 }
