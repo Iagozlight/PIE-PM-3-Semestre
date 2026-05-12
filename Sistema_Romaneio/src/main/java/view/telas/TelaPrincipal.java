97a54fb211416f613088a52725bee56bcd5d048d
iagozlight <iago.chagas@escola.pr.gov.br>
2026-05-11 21:24:03 -0300

    comecei a funcionalidade do cursor, e enviei algumas imagens, porém, a maioria não funcionou, estou tentando ver alguma que funcione. Adicionei o LoadCuror em TelaPrincipal.java, e ajustei um pouco o código

--- Sistema_Romaneio/src/main/java/view/telas/TelaPrincipal.java
+++ Sistema_Romaneio/src/main/java/view/telas/TelaPrincipal.java
@@ -2,28 +2,41 @@ package view.telas;
 
 import javax.swing.*;
 import java.awt.*;
+import java.net.URL;
 
-public class TelaPrincipal{
+public class TelaPrincipal {
 
-
-
-    public TelaPrincipal(){
-        //--------- DEFINIÇÕES ---------
+    public static void main(String[] args) {
         JFrame frame = new JFrame("Tela Principal");
         frame.setSize(800, 600);
         frame.setLayout(new FlowLayout());
+        frame.getContentPane().setBackground(new Color(0xF5F0E0));
 
         JPanel panel = new JPanel();
         panel.setLayout(new FlowLayout(FlowLayout.CENTER));
 
-
-        //------ ADDS -------
-
+        frame.setCursor(loadCursor("/view/cursor/cursor.png"));
         frame.add(panel);
-
-
 
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setVisible(true);
+    }
+
+    private static Cursor loadCursor(String resourcePath) {
+        URL cursorUrl = TelaPrincipal.class.getResource(resourcePath);
+        if (cursorUrl == null) {
+            return Cursor.getDefaultCursor();
+        }
+
+        Image image = new ImageIcon(cursorUrl).getImage();
+        try {
+            return Toolkit.getDefaultToolkit().createCustomCursor(
+                    image,
+                    new Point(16, 16),
+                    "cursor"
+            );
+        } catch (RuntimeException ex) {
+            return Cursor.getDefaultCursor();
+        }
     }
 }
