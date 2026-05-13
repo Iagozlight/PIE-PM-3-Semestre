cc93a35cbf59663c711e25b292cd4bcc5678fe95
Thales <thalesantos12@gmail.com>
2026-05-12 21:19:06 -0300

    add: Criei a PainelRodape, mesmo estilo da PainelTopo, mas com funcionalidades do atribuir veicul e motorista e deletar

---
+++ Sistema_Romaneio/src/main/java/projeto/views/componentes/PainelRodape.java
@@ -0,0 +1,44 @@
+package projeto.views.componentes;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class PainelRodape extends JPanel {
+
+    private JButton btnAtribuirVeiculo;
+    private JButton btnAtribuirMotorista;
+    private JButton btnDeletar;
+
+    private final Color corFundo = new Color(245, 240, 225);
+    private final Color corMarrom = new Color(60, 42, 33);
+    private final Color corBranco = new Color(252, 249, 241);
+
+    public PainelRodape() {
+        setLayout(new FlowLayout(FlowLayout.LEFT));
+        setBackground(corFundo);
+        setBorder(BorderFactory.createEmptyBorder(5, 15, 5, 15));
+        iniciar();
+    }
+
+    private void iniciar() {
+        btnAtribuirVeiculo = new JButton("Atribuir Veículo");
+        btnAtribuirVeiculo.setBackground(corBranco);
+        btnAtribuirVeiculo.setForeground(corMarrom);
+
+        btnAtribuirMotorista = new JButton("Atribuir Motorista");
+        btnAtribuirMotorista.setBackground(corBranco);
+        btnAtribuirMotorista.setForeground(corMarrom);
+
+        btnDeletar = new JButton("Deletar");
+        btnDeletar.setBackground(new Color(211, 47, 47));
+        btnDeletar.setForeground(Color.WHITE);
+
+        add(btnAtribuirVeiculo);
+        add(btnAtribuirMotorista);
+        add(btnDeletar);
+    }
+
+    public JButton getBtnAtribuirVeiculo() { return btnAtribuirVeiculo; }
+    public JButton getBtnAtribuirMotorista() { return btnAtribuirMotorista; }
+    public JButton getBtnDeletar() { return btnDeletar; }
+}
